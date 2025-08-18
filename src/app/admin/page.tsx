3e85e8be85f8c33d34020ac0efdd3dc40b1445b1
zeeshan <chzeeshanszforever099@gmail.com>
2025-08-18 13:56:52 +0500

    Refactor: Remove unused components and tables from admin dashboard
    - Deleted proposal history table and its associated columns.
    - Removed referrals page and its related components.
    - Cleaned up user management components including transaction history and KYC/AML sections.
    - Removed unnecessary imports and unused code to streamline the admin dashboard.
    - Added a new index file for the admin module.

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Admin from '@/modules/admin/index'
+const page = () => {
+    return (
+        <>
+            <Admin />
+        </>
+    )
+}
+
+export default page
